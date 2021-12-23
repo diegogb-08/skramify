434909192996e3beb29d87213c8823389ae6be80
Diego <diego.garciabrisa@maibornwolff.de>
2021-12-23 21:46:03 +0100

    feat(image): add profile image

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import AuthenticationButton from '../button/AuthenticationButton';
 
 export type Menu = {
-  isMobile: boolean
+  isMobile?: boolean
 }
 
 const Menu = ({ isMobile }: Menu) => {
@@ -14,18 +14,17 @@ const Menu = ({ isMobile }: Menu) => {
   } = useAuth0();
 
   return (
-    <div className='container w-full h-12'>
-      <div className='bg-blue-400 w-full h-full flex justify-end items-center pr-4'>
+    <div className='container max-w-full h-16 overflow-hidden'>
+      <div className='bg-blue-400 w-full h-full flex justify-end items-center p-2'>
         {
           isAuthenticated ?
-            <div>
+            <>
               Hello {user?.nickname}
+              <img className='h-full rounded-full ml-2' src={user?.picture!} alt='Profile picture' />
               <AuthenticationButton onClick={() => logout({ returnTo: window.location.origin })} text='Log out' />
-            </div>
+            </>
             :
-            <div>
-              <AuthenticationButton onClick={loginWithRedirect} text='Log in' />
-            </div>
+            <AuthenticationButton onClick={loginWithRedirect} text='Log in' />
         }
       </div>
     </div>
