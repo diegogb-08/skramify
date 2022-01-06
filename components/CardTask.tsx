630793dd1af3b78ad94b1573c9b727cfbb9dd0e7
Diego <diego.garciabrisa@maibornwolff.de>
2022-01-06 09:51:18 +0100

    feat(CardTask): WIP create component

--- components/CardTask.tsx
+++ components/CardTask.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { Descendant } from "slate"
 import { TaskCard } from "../types"
 
@@ -6,11 +7,15 @@ interface CardTaskProps extends TaskCard {
 
 const CardTask = ({ title, cardType, comments, createdAt, description, id, priority, dueDate }: CardTaskProps) => {
   return (
-    <div>
+    <div className='bg-white rounded-sm overflow-hidden ring-1 ring-gray-900/5 p-4 m-2 text-inherit font-light'>
+      <Link href={`board/${id}`}>
+        <a className='font-normal text-gray-500 hover:underline'>{id}</a>
+      </Link>
       <p>{title}</p>
-      <p>{id}</p>
-      <p>{cardType}</p>
-      <p>{priority}</p>
+      <div className='w-full flex flex-row justify-between'>
+        <p>{cardType}</p>
+        <p>{priority}</p>
+      </div>
       <p>{dueDate}</p>
     </div>
   )
